fdae8d1343c8c5469b3923ffa0eae6de7a6578fb
EdgarVERMECH <edgarvermech@icloud.com>
2023-02-13 09:13:03 +0100

    init

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': 'Buy groceries',
+        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Learn Python',
+        'description': 'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        return jsonify({'message': 'Task not found'})
+    return jsonify({'task': task[0]})
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        return jsonify({'message': 'Bad request'})
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        return jsonify({'message': 'Task not found'})
+    if not request.json:
+        return jsonify({'message': 'Bad request'})
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        return jsonify({'message': 'Task not found'})
+    tasks.remove(task[0])
+    return jsonify({'message': 'Task deleted'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
